c7a2b65b71e806e5d50812f27c9c585e32646188
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-05-20 15:55:33 +0200

    changement_login.php et changement_mdp.php finit. Reste CSS.

--- profil.php
+++ profil.php
@@ -3,7 +3,9 @@ session_start();
 if ($_SESSION['login']) {
 	echo "Bienvenue ".$_SESSION['login']. " ! <br/><a href='logout.php'>Se déconnecter</a><br/>
 
-	<a href='changement.php'>Changer de mot de passe</a>";
+	<a href='changement.php'>Changer de mot de passe</a><br/>
+
+	<a href='changement_login.php'>Changer de login</a>";
 }
 else
 {
